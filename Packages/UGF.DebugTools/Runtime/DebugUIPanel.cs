b5631b55f597bf630821490f1ddacd501f8570a6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-10 22:51:09 +0300

    Update DebugUIPanel.cs

--- Packages/UGF.DebugTools/Runtime/DebugUIPanel.cs
+++ Packages/UGF.DebugTools/Runtime/DebugUIPanel.cs
@@ -4,9 +4,11 @@ namespace UGF.DebugTools.Runtime
 {
     public abstract class DebugUIPanel
     {
+        public bool Enabled { get; set; } = true;
         public Vector3 Position { get; set; }
         public Vector2 Size { get; set; } = Vector2.one * 100F;
         public Rect Rect { get; private set; }
+        public bool IsVisible { get; private set; }
 
         public void Enable()
         {
@@ -20,18 +22,32 @@ namespace UGF.DebugTools.Runtime
 
         public void DrawGUI()
         {
+            if (IsVisible)
+            {
+                GUILayout.BeginArea(Rect);
+
+                OnDrawGUILayout();
+
+                GUILayout.EndArea();
+            }
+
             if (Event.current.type == EventType.Repaint)
             {
                 Vector3 screenPoint = Camera.current.WorldToScreenPoint(Position);
 
-                Rect = new Rect(screenPoint.x, Screen.height - screenPoint.y, Size.x, Size.y);
+                if (screenPoint.z > 0F)
+                {
+                    var rect = new Rect(screenPoint.x, Screen.height - screenPoint.y, Size.x, Size.y);
+                    var screen = new Rect(0F, 0F, Screen.width, Screen.height);
+
+                    Rect = rect;
+                    IsVisible = screen.Contains(screenPoint);
+                }
+                else
+                {
+                    IsVisible = false;
+                }
             }
-
-            GUILayout.BeginArea(Rect);
-
-            OnDrawGUILayout();
-
-            GUILayout.EndArea();
         }
 
         protected virtual void OnEnable()
