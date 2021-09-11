02c000ebd3f7adb18c26381f5aa5ff7aecd9968f
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-11 15:03:59 +0300

    Add DebugSettings

---
+++ Packages/UGF.DebugTools/Editor/DebugSettingsProvider.cs
@@ -0,0 +1,15 @@
+﻿using UGF.CustomSettings.Editor;
+using UGF.DebugTools.Runtime;
+using UnityEditor;
+
+namespace UGF.DebugTools.Editor
+{
+    internal static class DebugSettingsProvider
+    {
+        [SettingsProvider]
+        private static SettingsProvider GetProvider()
+        {
+            return new CustomSettingsProvider<DebugSettingsAsset>("Project/Unity Game Framework/Debug", DebugSettings.Settings, SettingsScope.Project);
+        }
+    }
+}
---
+++ Packages/UGF.DebugTools/Runtime/DebugSettings.cs
@@ -0,0 +1,13 @@
+﻿using UGF.CustomSettings.Runtime;
+
+namespace UGF.DebugTools.Runtime
+{
+    public static class DebugSettings
+    {
+        public static CustomSettingsPackage<DebugSettingsAsset> Settings { get; } = new CustomSettingsPackage<DebugSettingsAsset>
+        (
+            "UGF.DebugTools",
+            nameof(DebugSettings)
+        );
+    }
+}
