d53450323e7144a70989765d63a5eeaa3ffd8a83
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-11 15:50:45 +0300

    Update TestDebugUIDrawerComponent.cs

--- Assets/UGF.DebugTools.Runtime.Tests/TestDebugUIDrawerComponent.cs
+++ Assets/UGF.DebugTools.Runtime.Tests/TestDebugUIDrawerComponent.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UGF.DebugTools.Runtime.Scopes;
+using UnityEngine;
 
 namespace UGF.DebugTools.Runtime.Tests
 {
@@ -8,6 +9,9 @@ namespace UGF.DebugTools.Runtime.Tests
 
         private class TestPanel : DebugUIPanelText
         {
+            private Vector2 m_scroll;
+            private float m_slider;
+
             public TestPanel()
             {
                 Scale = Vector2.one * 2F;
@@ -17,7 +21,32 @@ namespace UGF.DebugTools.Runtime.Tests
             {
                 base.OnDrawGUILayout();
 
+                using (new DebugUIVerticalScope(GUIContent.none, GUI.skin.box))
+                using (var view = new DebugUIScrollViewScope(m_scroll))
+                {
+                    OnDrawElements();
+
+                    m_scroll = view.ScrollPosition;
+                }
+            }
+
+            private void OnDrawElements()
+            {
                 GUILayout.Button("Button");
+                GUILayout.Label("Label");
+                GUILayout.TextField("TextField");
+                GUILayout.Box("Box");
+                GUILayout.Toolbar(1, new[] { "Tab1", "Tab2" });
+                GUILayout.Toggle(true, "Toggle");
+                GUILayout.PasswordField("Password", '*');
+                GUILayout.TextArea("TextArea");
+                GUILayout.SelectionGrid(1, new[] { "Cell1", "Cell2", "Cell3", "Cell4" }, 2);
+
+                m_slider = GUILayout.HorizontalSlider(m_slider, 0, 1);
+                m_slider = GUILayout.VerticalSlider(m_slider, 0, 1);
+
+                m_slider = GUILayout.HorizontalScrollbar(m_slider, 0.25f, 0, 1);
+                m_slider = GUILayout.VerticalScrollbar(m_slider, 0.25f, 0, 1);
             }
         }
 
