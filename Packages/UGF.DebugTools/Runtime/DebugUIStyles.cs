a74a088d37c7d474ccb89bd9979069048e84dce5
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-17 18:23:44 +0300

    Add DebugUIStyles

---
+++ Packages/UGF.DebugTools/Runtime/DebugUIStyles.cs
@@ -0,0 +1,10 @@
+﻿namespace UGF.DebugTools.Runtime
+{
+    public static class DebugUIStyles
+    {
+        public static string Frame { get; } = "frame";
+        public static string FrameHighlight { get; } = "highlight";
+        public static string CheckMark { get; } = "arrow";
+        public static string Foldout { get; } = "foldout";
+    }
+}
