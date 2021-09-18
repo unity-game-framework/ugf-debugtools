06c0d105abc9ae0edb601a141d28015f200f39fa
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-18 16:13:03 +0300

    Rework styles

--- Packages/UGF.DebugTools/Runtime/DebugUIStyles.cs
+++ Packages/UGF.DebugTools/Runtime/DebugUIStyles.cs
@@ -5,7 +5,7 @@
         public static string Header { get; } = "header";
         public static string Frame { get; } = "frame";
         public static string FrameHighlight { get; } = "highlight";
-        public static string CheckMark { get; } = "arrow";
+        public static string CheckMark { get; } = "checkmark";
         public static string Foldout { get; } = "foldout";
     }
 }
