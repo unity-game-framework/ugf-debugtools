47315777a7cceb46d62913ff6d485c506673db9b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-18 21:00:56 +0300

    Add panels display function

--- Packages/UGF.DebugTools/Runtime/UI.Panels/DebugUIPanelDrawer.cs
+++ Packages/UGF.DebugTools/Runtime/UI.Panels/DebugUIPanelDrawer.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using UGF.DebugTools.Runtime.UI.Functions;
 
 namespace UGF.DebugTools.Runtime.UI.Panels
 {
     public class DebugUIPanelDrawer : DebugUIDrawerBase
     {
+        public bool Display { get; set; }
         public IReadOnlyCollection<DebugUIPanel> Panels { get; }
 
         private readonly List<DebugUIPanel> m_panels = new List<DebugUIPanel>();
+        private DebugUIFunction m_functionDisplay;
 
         public DebugUIPanelDrawer()
         {
@@ -49,14 +52,38 @@ namespace UGF.DebugTools.Runtime.UI.Panels
             m_panels.Clear();
         }
 
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            m_functionDisplay = DebugUI.AddFunction(DebugUI.DebugFunctionGroupName, "Panels Display", OnFunctionDisplay);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            DebugUI.RemoveFunction(DebugUI.DebugFunctionGroupName, m_functionDisplay);
+        }
+
         protected override void OnDrawGUI()
         {
-            for (int i = 0; i < m_panels.Count; i++)
+            if (Display)
             {
-                DebugUIPanel panel = m_panels[i];
+                for (int i = 0; i < m_panels.Count; i++)
+                {
+                    DebugUIPanel panel = m_panels[i];
 
-                panel.DrawGUI();
+                    panel.DrawGUI();
+                }
             }
+        }
+
+        private void OnFunctionDisplay(DebugUIFunction function)
+        {
+            Display = !Display;
+
+            function.Enabled = Display;
         }
     }
 }
--- Packages/UGF.DebugTools/Runtime/UI.Panels/DebugUIPanelDrawerAsset.cs
+++ Packages/UGF.DebugTools/Runtime/UI.Panels/DebugUIPanelDrawerAsset.cs
@@ -5,9 +5,16 @@ namespace UGF.DebugTools.Runtime.UI.Panels
     [CreateAssetMenu(menuName = "Unity Game Framework/Debug/Debug UI Panel Drawer", order = 2000)]
     public class DebugUIPanelDrawerAsset : DebugUIDrawerAsset
     {
+        [SerializeField] private bool m_display;
+
+        public bool Display { get { return m_display; } set { m_display = value; } }
+
         protected override IDebugUIDrawer OnBuild()
         {
-            return new DebugUIPanelDrawer();
+            return new DebugUIPanelDrawer
+            {
+                Display = m_display
+            };
         }
     }
 }
