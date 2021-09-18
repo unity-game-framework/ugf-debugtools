7c113d933c5ff5d3f580e280fd52fbf1886f3caa
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-18 17:03:11 +0300

    Add indent scopes

---
+++ Packages/UGF.DebugTools/Runtime/UI.Scopes/DebugUIIndentIncrementScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UGF.DebugTools.Runtime.UI.Scopes
+{
+    public readonly struct DebugUIIndentIncrementScope : IDisposable
+    {
+        private readonly int m_indent;
+
+        public DebugUIIndentIncrementScope(int value)
+        {
+            m_indent = DebugUI.IndentLevel;
+
+            DebugUI.IndentLevel += value;
+        }
+
+        public void Dispose()
+        {
+            DebugUI.IndentLevel = m_indent;
+        }
+    }
+}
