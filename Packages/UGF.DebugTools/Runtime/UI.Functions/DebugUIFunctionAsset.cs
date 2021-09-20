c33e4671d2083045dd425c0085b215cbd336adef
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-20 17:35:22 +0300

    Update DebugUIFunctionAsset.cs

--- Packages/UGF.DebugTools/Runtime/UI.Functions/DebugUIFunctionAsset.cs
+++ Packages/UGF.DebugTools/Runtime/UI.Functions/DebugUIFunctionAsset.cs
@@ -6,9 +6,7 @@ namespace UGF.DebugTools.Runtime.UI.Functions
     public abstract class DebugUIFunctionAsset : BuilderAsset<DebugUIFunction>
     {
         [SerializeField] private string m_groupName = "Debug";
-        [SerializeField] private string m_label = "Function";
 
         public string GroupName { get { return m_groupName; } set { m_groupName = value; } }
-        public string Label { get { return m_label; } set { m_label = value; } }
     }
 }
