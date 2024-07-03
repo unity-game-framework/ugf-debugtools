5f8c8b3317666608c10a2d5c231b0ff378cbe352
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-07-03 15:28:36 +0300

    Add DebugUISectionLog

---
+++ Packages/UGF.DebugTools/Runtime/UI.Sections.Misc/DebugUISectionLogData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UGF.DebugTools.Runtime.UI.Sections.Misc
+{
+    public struct DebugUISectionLogData
+    {
+        public bool Foldout { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public LogType Type { get; set; }
+        public string Message { get; set; }
+    }
+}
