828e29b3a0cf4e4b64c7cc556fdde3878dd1bfef
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-08-24 17:53:22 +0300

    Add tests

---
+++ Assets/UGF.DebugTools.Runtime.Tests/TestDebugUIElementAsset.cs
@@ -0,0 +1,27 @@
+﻿using UGF.DebugTools.Runtime.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UGF.DebugTools.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestDebugUIElementAsset")]
+    public class TestDebugUIElementAsset : DebugUIElementAsset
+    {
+        [SerializeField] private string m_message;
+
+        public string Message { get { return m_message; } set { m_message = value; } }
+
+        protected override DebugUIElement OnBuild()
+        {
+            return new TestDebugUIElement(m_message);
+        }
+    }
+
+    public class TestDebugUIElement : DebugUIElement
+    {
+        public TestDebugUIElement(string message)
+        {
+            Add(new Label(message));
+        }
+    }
+}
