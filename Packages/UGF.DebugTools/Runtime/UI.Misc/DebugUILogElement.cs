42ecb4a3bf9cd906d7945dd920826fbda5282432
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-08-26 20:01:54 +0300

    Add DebugUILogElement

---
+++ Packages/UGF.DebugTools/Runtime/UI.Misc/DebugUILogElement.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UGF.DebugTools.Runtime.UI.Misc
+{
+    public class DebugUILogElement : DebugUIElement
+    {
+        private readonly List<DebugUILogEntryData> m_entries = new List<DebugUILogEntryData>();
+        private readonly ListView m_list;
+
+        public DebugUILogElement()
+        {
+            var buttonClear = new Button
+            {
+                text = "Clear",
+            };
+
+            m_list = new ListView
+            {
+                itemsSource = m_entries,
+                makeItem = OnListItemCreate,
+                bindItem = OnListItemBind,
+                unbindItem = OnListItemUnbind,
+                virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight
+            };
+
+            Add(buttonClear);
+            Add(m_list);
+
+            buttonClear.clicked += OnClearClicked;
+
+            RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
+        }
+
+        private void OnAttachToPanel(AttachToPanelEvent _)
+        {
+            Application.logMessageReceived += OnMessageReceived;
+        }
+
+        private void OnDetachFromPanel(DetachFromPanelEvent _)
+        {
+            Application.logMessageReceived -= OnMessageReceived;
+        }
+
+        private VisualElement OnListItemCreate()
+        {
+            return new DebugUILogEntryElement();
+        }
+
+        private void OnListItemBind(VisualElement element, int index)
+        {
+            var entryElement = (DebugUILogEntryElement)element;
+
+            entryElement.Bind(m_entries[index]);
+        }
+
+        private void OnListItemUnbind(VisualElement element, int index)
+        {
+            var entryElement = (DebugUILogEntryElement)element;
+
+            entryElement.Unbind();
+        }
+
+        private void OnClearClicked()
+        {
+            m_entries.Clear();
+            m_list.RefreshItems();
+        }
+
+        private void OnMessageReceived(string condition, string stacktrace, LogType type)
+        {
+            m_entries.Add(new DebugUILogEntryData(
+                DateTime.Now,
+                type,
+                condition,
+                stacktrace
+            ));
+        }
+    }
+}
---
+++ Packages/UGF.DebugTools/Runtime/UI.Misc/DebugUILogEntryData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.DebugTools.Runtime.UI.Misc
+{
+    public readonly struct DebugUILogEntryData
+    {
+        public DateTime Time { get; }
+        public LogType Type { get; }
+        public string Message { get; }
+        public string Body { get; }
+
+        public DebugUILogEntryData(DateTime time, LogType type, string message, string body)
+        {
+            if (string.IsNullOrEmpty(message)) throw new ArgumentException("Value cannot be null or empty.", nameof(message));
+            if (string.IsNullOrEmpty(body)) throw new ArgumentException("Value cannot be null or empty.", nameof(body));
+
+            Time = time;
+            Type = type;
+            Message = message;
+            Body = body;
+        }
+    }
+}
